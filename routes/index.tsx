5183058ab2e04487d89d40c05aec6d208568c0a0
oliver <kookooloopdaking@gmail.com>
2022-08-18 18:30:44 -0500

    init

---
+++ routes/index.tsx
@@ -0,0 +1,10 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+export default function Home() {
+  return (
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+    </div>
+  );
+}
